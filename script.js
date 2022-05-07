86bf0f25be7776c8a6a174d4c49559f82f27d14a
jonathan <jonathansanfelipe1996@gmail.com>
2022-05-07 16:42:32 -0430

    actualizado

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// escritura automática
+
+
+let typed = new Typed(".textojs",{
+    strings:["Jhonatan", "web Developer", "UI Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
